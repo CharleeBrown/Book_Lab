ad85c1a1dba932dc85fb7e3a196a4bb592c450a0
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-10-29 15:27:43 -0500

    Everything is working on the UI.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var bookingsRouter = require('./routes/bookings');
+let listsRouter = require('./routes/lists');
 
 var app = express();
 
@@ -29,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/bookings', bookingsRouter);
-
+app.use('/lists', listsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
