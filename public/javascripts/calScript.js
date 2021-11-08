096b49ea55bd12573bb944926e8bdf4caa368b8e
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-11-08 10:48:40 -0600

    Fixed the issue of items showing up on calendar.

--- public/javascripts/calScript.js
+++ public/javascripts/calScript.js
@@ -1,24 +1,43 @@
-let x = 1;
-let cal = new tui.Calendar('#calendar', {
-	defaultView: 'month', // monthly view option
-	taskView: ['task'],
-	scheduleView: false
-});
-
-function getDates(url){
-	fetch(url, 
-		{headers: 
-			{'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		}, 
-	method: 'POST'})
-	.then(response => response.json())
-	.then(data=>{setDates(data)});
+function getDates(url) {
+	fetch(url, {
+			headers: {'Accept': 'application/json','Content-Type': 'application/json'},
+			method: 'POST'
+				})
+		.then(response => response.json())
+		.then(data => {
+			let x = 1;
+			let newArray = [];
+			let newdata = JSON.stringify(data);
+			let jsonres = JSON.parse(newdata);
+			for (let i = 0; i < jsonres.length; i++) {
+				let newObj = {
+					'id': x.toString(),
+					calendarId: x.toString(),
+					title: jsonres[i]['name'],
+					category: 'time',
+					dueDateClass: '',
+					start: jsonres[i]['startTime'],
+					end: jsonres[i]['stopTime']
+				};
+			newArray.push(newObj);
+				x += 1;
+			};
+			let cal = new tui.Calendar('#calendar', {
+				defaultView: 'month', // monthly view option
+				scheduleView: true,
+				useCreationPopup:false,
+				useDetailPopup:false,
+			});
+			cal.createSchedules(newArray);
+			console.log(newArray);
+			//createDates(data
+		});
 };
 
-async function setDates(sets) {
+function setDates(sets) {
 	let newCal = document.getElementById('calendar');
 	console.log(sets);
+	let x = 1;
 	let newArray = [];
 	for (let i in sets) {
 		let newObj = {
@@ -30,22 +49,31 @@ async function setDates(sets) {
 			start: sets[i].startTime,
 			end: sets[i].stopTime
 		};
-		x+=1;
-		
+		x += 1;
 		newArray.push(newObj);
-		
 	}
-	sessionStorage.setItem('schedule', JSON.stringify(newArray));
 
-	cal.createSchedules(JSON.parse(sessionStorage.getItem('schedule')));
-	console.log("test");
-	console.log("test");
-	let newa = JSON.parse(await sessionStorage.getItem('schedule'));
-	console.log(newa);
-	// console.log(newArray);
+	let cal = new tui.Calendar('#calendar', {
+		defaultView: 'month', // monthly view option
+		taskView: ['task'],
+		scheduleView: false
+	});
+	cal.createSchedules;
+	// sessionStorage.setItem('schedule', JSON.stringify(newArray));
+
+	// cal.createSchedules(JSON.parse(sessionStorage.getItem('schedule')));
+	// console.log("test");
+	// console.log("test");
+	// let newa = JSON.parse(await sessionStorage.getItem('schedule'));
+	// console.log(newa);
+	// // console.log(newArray);
 	// return newArray;
-};
+}
 
+function createDates(dates) {
+
+
+}
 
 // function getDates(url, func) {
 // 	let xht = new XMLHttpRequest();
--- routes/bookings.js
+++ routes/bookings.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var router = express.Router();
 let config = require('./configs');
-const { MongoClient } = require("mongodb");
+const {
+  MongoClient
+} = require("mongodb");
 const uri = config.connString;
-const client = new MongoClient(uri,{
+const client = new MongoClient(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology:true,
+  useUnifiedTopology: true,
 });
 const collect = client.db("mainDB").collection("CalLabBooking");
 
 /* GET bookings listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 
-router.post('/',function(req,res,next){
+router.post('/', function (req, res, next) {
   console.log("Arrived");
   let query = {
     bookDate: req.body.labDate,
@@ -31,23 +33,22 @@ router.post('/',function(req,res,next){
   };
   client.connect(async (err) => {
     console.log("starting to wait");
-   return await collect.find(query) // TODO: Work on sending the request to the database.
+    return await collect.find(query) // TODO: Work on sending the request to the database.
       .sort()
       .toArray()
       .catch(err => console.log(err))
       .then((items) => {
-        if(items.length <= 0){
+        if (items.length <= 0) {
           console.log(items);
-        collect.insertOne(obj);
-      }
-      else{
-        console.log("Match found, select new date");
-        //res.status(200).redirect('/');
+          collect.insertOne(obj);
+        } else {
+          console.log("Match found, select new date");
+          //res.status(200).redirect('/');
         }
         return items;
       });
-    });
-    res.status(200).redirect('/lists');
+  });
+  res.status(200).redirect('/lists');
 });
 
 
